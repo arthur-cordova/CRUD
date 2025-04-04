464cf2f32f1a0da3fd53c8c204260c43e790cdfa
Arthur Cordova <arthurbscordova@gmail.com>
2025-04-04 19:40:30 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Erro");
+    }
+    console.log("Sistema Rodando");
+})
